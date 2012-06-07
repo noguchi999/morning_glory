f6011165880681599b6cb8e132b03fd25df9c642
Osamu Noguchi <noguchi999@gmail.com>
2012-06-07 20:56:04 +0900

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MorningGlory::Application.config.secret_token = '275356354603ad2ead175548ebe1e8166dd9e9f1542891f3d64d791e232e337b51aa4cad02015aa16d98ab9a8427e07daeb429889957d9c688a77a11c841bf37'
